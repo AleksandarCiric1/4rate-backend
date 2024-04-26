d3fe8b852c4c3388e8341da84c3f20a5bb84213a
AC <tojmp.rootsaver@gmail.com>
2024-04-26 23:45:33 +0200

    First commit to 4rate backend app!

---
+++ src/main/java/com/example/backend4rate/Backend4rateApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend4rate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Backend4rateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Backend4rateApplication.class, args);
+    }
+
+}
