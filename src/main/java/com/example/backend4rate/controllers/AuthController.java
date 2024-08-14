56ce178ac489fb21a4675249c4e00848c7de0875
GogsBogs <gordana.tubonjic@student.etf.unibl.org>
2024-08-14 21:22:00 +0200

    Pokusaj dva #

--- src/main/java/com/example/backend4rate/controllers/AuthController.java
+++ src/main/java/com/example/backend4rate/controllers/AuthController.java
@@ -15,9 +15,6 @@ import com.example.backend4rate.services.impl.ImageService;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-
-
 @RestController
 @RequestMapping("/v1/auth")
 public class AuthController {
