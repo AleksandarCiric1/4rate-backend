e1ec54dede347b05fc930241bfcab909ac3b356d
Danica Delić <87670724+DanicaDelic@users.noreply.github.com>
2024-09-12 03:29:32 +0200

    Update RestaurantServiceInterface

--- src/main/java/com/example/backend4rate/services/RestaurantServiceInterface.java
+++ src/main/java/com/example/backend4rate/services/RestaurantServiceInterface.java
@@ -1,1 +1,17 @@
+package com.example.backend4rate.services;
 
+import com.example.backend4rate.models.dto.Restaurant;
+import com.example.backend4rate.models.entities.CategoryEntity;
+
+import java.util.List;
+
+public interface RestaurantServiceInterface {
+    List<Restaurant> searchRestaurant(String name);
+    Restaurant getRestaurant(int idRestaurant);
+    List<Restaurant> getAllRestaurants();
+    List<Restaurant> getFavoriteRestaurants();
+    boolean addFavoriteRestaurant(int idRestaurant);
+    boolean updateRestaurantInformation(Restaurant restaurant);
+    boolean removeFavoriteRestaurant(int idRestaurant);
+    List<Restaurant> getAllRestaurants(List<CategoryEntity> categories);
+}
