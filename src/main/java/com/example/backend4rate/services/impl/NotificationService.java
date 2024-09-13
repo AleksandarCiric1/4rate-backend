80dc26a987d130d2c634cea699be55bea484c390
Danica Delić <87670724+DanicaDelic@users.noreply.github.com>
2024-09-14 01:30:32 +0200

    NotificationService updated

--- src/main/java/com/example/backend4rate/services/impl/NotificationService.java
+++ src/main/java/com/example/backend4rate/services/impl/NotificationService.java
@@ -1,1 +1,23 @@
+package com.example.backend4rate.services.impl;
 
+import com.example.backend4rate.exceptions.NotFoundException;
+import com.example.backend4rate.models.entities.NotificationEntity;
+import com.example.backend4rate.repositories.NotificationRepository;
+import com.example.backend4rate.services.NotificationServiceInterface;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class NotificationService implements NotificationServiceInterface {
+
+    private final NotificationRepository notificationRepository;
+
+    public NotificationService(NotificationRepository notificationRepository) {
+        this.notificationRepository = notificationRepository;
+    }
+
+    @Override
+    public List<NotificationEntity> getNotificationsForGuest(Integer guestId) {
+        return notificationRepository.findByGuestId(guestId);
+    }
