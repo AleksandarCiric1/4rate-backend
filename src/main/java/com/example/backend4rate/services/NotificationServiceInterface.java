b7b572d5114ed1fbd4917772f762a112ba388216
Danica Delić <87670724+DanicaDelic@users.noreply.github.com>
2024-09-14 01:28:54 +0200

    NotificationServiceInterface updated

--- src/main/java/com/example/backend4rate/services/NotificationServiceInterface.java
+++ src/main/java/com/example/backend4rate/services/NotificationServiceInterface.java
@@ -1,1 +1,10 @@
+package com.example.backend4rate.services;
+
+import com.example.backend4rate.models.entities.NotificationEntity;
+import java.util.List;
+
+public interface NotificationServiceInterface {
+
+  
+}
 
