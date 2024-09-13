a13ada66c3d2b9391f0f50808b0212dc878486a1
Danica Delić <87670724+DanicaDelic@users.noreply.github.com>
2024-09-14 01:17:56 +0200

    CategorySubscriptionRepository updated

--- src/main/java/com/example/backend4rate/repositories/CategorySubscriptionRepository.java
+++ src/main/java/com/example/backend4rate/repositories/CategorySubscriptionRepository.java
@@ -1,1 +1,18 @@
+package com.example.backend4rate.repositories;
+
+import com.example.backend4rate.models.entities.CategorySubscriptionEntity;
+import com.example.backend4rate.models.entities.CategoryEntity;
+import com.example.backend4rate.models.entities.GuestEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CategorySubscriptionRepository extends JpaRepository<CategorySubscriptionEntity, Integer> {
+    List<CategorySubscriptionEntity> findByCategory(CategoryEntity category);
+    CategorySubscriptionEntity findByGuestIdAndCategoryId(GuestEntity guest, CategoryEntity category);
+    Optional<CategorySubscriptionEntity> findByGuestIdAndCategoryId(Integer guestId, Integer categoryId);
+}
 
