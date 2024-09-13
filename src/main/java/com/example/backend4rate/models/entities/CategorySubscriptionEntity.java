e216cca7c7f29f3c055756ac2ae22467a2d7c672
Danica Delić <87670724+DanicaDelic@users.noreply.github.com>
2024-09-14 01:21:48 +0200

    CategorySubscriptionEntity updated

--- src/main/java/com/example/backend4rate/models/entities/CategorySubscriptionEntity.java
+++ src/main/java/com/example/backend4rate/models/entities/CategorySubscriptionEntity.java
@@ -1,12 +1,14 @@
 package com.example.backend4rate.models.entities;
 
+import com.example.backend4rate.base.BaseEntity;
+
 import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
 @Table(name = "category_subscription")
 @Entity
-public class CategorySubscriptionEntity {
+public class CategorySubscriptionEntity implements BaseEntity<Integer>{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,4 +22,7 @@ public class CategorySubscriptionEntity {
     @ManyToOne
     @JoinColumn(name = "guest_id", referencedColumnName = "id", nullable = false)
     private GuestEntity guest;
+    
+    @Column(name = "subscribed", nullable = false)
+    private boolean subscribed;
 }
