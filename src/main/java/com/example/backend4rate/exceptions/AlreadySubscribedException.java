4d42a0edebd6d97e533179aaae2a3d960d4d2873
Danica Delić <87670724+DanicaDelic@users.noreply.github.com>
2024-09-14 20:38:05 +0200

    updated

--- src/main/java/com/example/backend4rate/exceptions/AlreadySubscribedException.java
+++ src/main/java/com/example/backend4rate/exceptions/AlreadySubscribedException.java
@@ -4,4 +4,7 @@ public class AlreadySubscribedException extends RuntimeException {
     public AlreadySubscribedException(String message) {
         super(message);
     }
+    public AlreadySubscribedException() {
+        super("Already subscribed to category");
+    }
 }
