cb3a40fe63402e08797fd0c915ca65f766b1cd78
Danica Delić <87670724+DanicaDelic@users.noreply.github.com>
2024-09-14 20:29:01 +0200

    CategoryNotFoundException updated

--- src/main/java/com/example/backend4rate/exceptions/CategoryNotFoundException.java
+++ src/main/java/com/example/backend4rate/exceptions/CategoryNotFoundException.java
@@ -4,4 +4,8 @@ public class CategoryNotFoundException extends RuntimeException {
     public CategoryNotFoundException(String message) {
         super(message);
     }
+
+    public NotFoundException() {
+        super("UserAccount is not found");
+    }
 }
