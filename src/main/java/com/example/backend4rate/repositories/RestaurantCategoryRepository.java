53f8f9b5c7665d24c5abd2790af3d61e0b8562a2
Danica Delić <87670724+DanicaDelic@users.noreply.github.com>
2024-09-14 13:40:45 +0200

    updated

---
+++ src/main/java/com/example/backend4rate/repositories/RestaurantCategoryRepository.java
@@ -0,0 +1,15 @@
+package com.example.backend4rate.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.example.backend4rate.models.entities.RestaurantCategoryEntity;
+import com.example.backend4rate.models.entities.CategoryEntity;
+
+import java.util.List;
+
+@Repository
+public interface RestaurantCategoryRepository extends JpaRepository<RestaurantCategoryEntity, Integer> {
+
+    List<RestaurantCategoryEntity> findByCategoryIn(List<CategoryEntity> categories);
+    
+}
