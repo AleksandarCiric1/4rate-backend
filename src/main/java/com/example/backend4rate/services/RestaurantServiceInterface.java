1479dbdc5efd48126aa102fede00468ba7f63180
Danica Delić <87670724+DanicaDelic@users.noreply.github.com>
2024-09-14 20:26:59 +0200

    RestaurantServiceInterface updated

--- src/main/java/com/example/backend4rate/services/RestaurantServiceInterface.java
+++ src/main/java/com/example/backend4rate/services/RestaurantServiceInterface.java
@@ -1,16 +1,20 @@
 package com.example.backend4rate.services;
 
 import com.example.backend4rate.models.dto.Restaurant;
+import com.example.backend4rate.exceptions.InvalidPhoneNumberException;
+import com.example.backend4rate.exceptions.DuplicatePhoneNumberException;
+
 import java.util.List;
 
 public interface RestaurantServiceInterface {
     List<Restaurant> getAllRestaurants();  
-    Restaurant getRestaurant(Integer restaurantId); 
+    Restaurant getRestaurant(Integer restaurantId);
     List<Restaurant> searchRestaurant(String name);  
-    boolean updateRestaurantInformation(Restaurant restaurant);  
-    Restaurant addRestaurant(Restaurant restaurant);  
+    boolean updateRestaurantInformation(Restaurant restaurant) throws InvalidPhoneNumberException, DuplicatePhoneNumberException;
+    Restaurant addRestaurant(Restaurant restaurant) throws InvalidPhoneNumberException, DuplicatePhoneNumberException;
     boolean addFavoriteRestaurant(Integer restaurantId, Integer guestId);  
     boolean removeFavoriteRestaurant(Integer restaurantId, Integer guestId);  
     List<Restaurant> getFavoriteRestaurants(Integer guestId); 
     List<Restaurant> getRestaurantsByCategories(List<Integer> categoryIds);
+ 
 }
