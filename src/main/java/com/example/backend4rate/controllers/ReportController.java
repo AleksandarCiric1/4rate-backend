c137bc3effd0caa9a4153de01525858566d180a4
AleksandarCiric1 <aleksandar.ciric@student.etf.unibl.org>
2024-09-19 13:10:40 +0200

    report controller modify

--- src/main/java/com/example/backend4rate/controllers/ReportController.java
+++ src/main/java/com/example/backend4rate/controllers/ReportController.java
@@ -10,6 +10,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,15 +23,16 @@ import com.example.backend4rate.services.impl.ReportService;
 @RestController
 @RequestMapping("/v1/reports")
 public class ReportController {
-private final ReportService reportService;
+    private final ReportService reportService;
 
-public ReportController(ReportService reportService){
-    this.reportService = reportService;
-}
+    public ReportController(ReportService reportService) {
+        this.reportService = reportService;
+    }
 
-  @GetMapping("/pdf/{restaurantId}")
-    public ResponseEntity<?> downloadPdf(@PathVariable Integer restaurantId,@RequestBody Report report) throws BadRequestException, NotFoundException, IOException{
-        try{
+    @PostMapping("/pdf/{restaurantId}")
+    public ResponseEntity<?> downloadPdf(@PathVariable Integer restaurantId, @RequestBody Report report)
+            throws BadRequestException, NotFoundException, IOException {
+        try {
             ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
 
             reportService.getReport(byteArrayOutputStream, restaurantId, report.getMonth(), report.getYear());
@@ -42,7 +44,7 @@ public ReportController(ReportService reportService){
             headers.setContentDispositionFormData("attachment", report.getMonth() + "_report.pdf");
 
             return ResponseEntity.ok().headers(headers).contentLength(resource.contentLength()).body(resource);
-        }catch (Exception e){
+        } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
         }
     }
